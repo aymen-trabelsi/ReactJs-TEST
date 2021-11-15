007956af8d82be24f3030d677366f51d369d90bf
Ahmed Touaiti <ahmed.touaiti.lpc@gmail.com>
2021-11-15 16:25:58 +0100

    add exemple call webservice

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/global.scss';
+import di from './di';
 
 function App() {
+
+  useEffect(() => {
+   di.partner.getPartners().then(data => console.log(data))
+   di.partnerCategory.getPartnersCategries().then(data => console.log(data))
+  }, [])
+
   return (
     <div className="App">
     </div>
