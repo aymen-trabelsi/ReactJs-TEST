93e4354903394648c72151c6de1a10ecac3614c7
Ahmed Touaiti <ahmed.touaiti.lpc@gmail.com>
2021-11-15 09:34:06 +0100

    add partners repository

---
+++ src/domains/repositories/PartnerRepository.ts
@@ -0,0 +1,19 @@
+import { IPartnerData, IPartnerEntity } from '@domains/entities/interfaces/iPartner'
+import Partner from '@domains/entities/Partner'
+import { axiosClient } from '@services/Http'
+import { IPartnerRepository } from './interfaces/IPartnerRepository'
+
+class PartnerRepository implements IPartnerRepository {
+
+  async getPartners(): Promise<Array<IPartnerEntity>> {
+    const response = await axiosClient.get("https://preprod.lepotcommuntest.fr/undefined/partners")
+
+    if(response?.data) {
+      return response.data.partners.map((partner: IPartnerData) => new Partner(partner))
+    }
+    return [];
+  }
+
+}
+
+export default PartnerRepository
